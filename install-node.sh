253c70f0375752374c5c2e5dcd64e29bfd7da252
Dilshad <98999149+a-rustacean@users.noreply.github.com>
2023-10-24 15:35:26 +0530

    Create install-node.sh

---
+++ install-node.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+sudo apt-get update -y 
+sudo apt-get upgrade -y
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+source ~/.bashrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" 
+nvm install --lts
